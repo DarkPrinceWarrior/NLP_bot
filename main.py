1da4c23e591cfd2ea45d5690f5e58e69fdfa386a
DarkPrinceWarrior <safaev.rus@gmail.com>
2021-12-08 23:56:01 +0300

    sequential model added

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import json
 
+
 import nltk_utils
 import numpy as np
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras import models
+from keras.utils.np_utils import to_categorical
 
 def jsonOpen():
     with open('intents.json', 'r') as f:
@@ -22,7 +31,7 @@ if __name__ == "__main__":
         for pattern in intent['patterns']:
             w = nltk_utils.tokenize(pattern)
             all_words.extend(w)
-            results.append((w,tag))
+            results.append((w, tag))
 
     ignore_words = ['.', '?', '!', ',']
 
@@ -43,6 +52,32 @@ if __name__ == "__main__":
         y_train.append(label)
 
     x_train = np.array(x_train)
-    y_train = np.array(y_train)
+    # y_train = np.array(y_train)
+
+    y_train = to_categorical(y_train, 7)
+
+
+    model = models.Sequential()
+    model.add(layers.Dense(8,activation="relu", input_shape=(len(all_words),)))
+    model.add(layers.Dense(8,activation="relu"))
+    model.add(layers.Dense(7,activation="softmax"))
+
+    model.compile(optimizer='rmsprop',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    x_val = x_train[:5]
+    partial_x_train = x_train[5:]
+
+    y_val = y_train[:5]
+    partial_y_train = y_train[5:]
+
+    history = model.fit(partial_x_train,
+                        partial_y_train,
+                        epochs=4,
+                        batch_size=8,
+                        validation_data=(x_val,y_val))
+
+    print(history)
 
 
