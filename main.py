b8af0c43a462cc434726a01b3ad712ec0af0d8dd
DarkPrinceWarrior <safaev.rus@gmail.com>
2021-12-08 21:14:19 +0300

    x_train and y_train data are ready

--- main.py
+++ main.py
@@ -1,11 +1,48 @@
+import json
+
 import nltk_utils
+import numpy as np
+
+def jsonOpen():
+    with open('intents.json', 'r') as f:
+        intents = json.load(f)
+    return intents
 
 
 if __name__ == "__main__":
-    sentence = "How long are you doing it?"
-    new_sentence = nltk_utils.tokenize(sentence)
-    new_sentence2 = list()
-    for element in new_sentence:
-        new_sentence2.append(nltk_utils.stemming(element))
-    new_sentence = new_sentence2
-    print(new_sentence)
+
+    all_words = []
+    tags = []
+    results = []
+
+    intents = jsonOpen()
+    for intent in intents['intents']:
+        tag = intent['tag']
+        tags.append(tag)
+        for pattern in intent['patterns']:
+            w = nltk_utils.tokenize(pattern)
+            all_words.extend(w)
+            results.append((w,tag))
+
+    ignore_words = ['.', '?', '!', ',']
+
+    all_words = [nltk_utils.stemming(w) for w in all_words
+                 if w not in ignore_words]
+    all_words = sorted(set(all_words))
+    tags = sorted(tags)
+
+    x_train = []
+    y_train = []
+
+    for (pattern_sentence, tag) in results:
+        # X: bag of words for each pattern_sentence
+        bag = nltk_utils.bag_of_words(pattern_sentence, all_words)
+        x_train.append(bag)
+        # y: PyTorch CrossEntropyLoss needs only class labels, not one-hot
+        label = tags.index(tag)
+        y_train.append(label)
+
+    x_train = np.array(x_train)
+    y_train = np.array(y_train)
+
+
--- nltk_utils.py
+++ nltk_utils.py
@@ -1,8 +1,11 @@
 import nltk
 
 # nltk.download('punkt') already downloaded
+import numpy as np
 from nltk.stem.porter import PorterStemmer
+
 stemmer = PorterStemmer()
+
 
 def tokenize(sentence):
     return nltk.word_tokenize(sentence)
@@ -12,8 +15,21 @@ def stemming(word):
     return stemmer.stem(word.lower())
 
 
-def excl_punk(sentence):
-    pass
-
 def bag_of_words(token_sentence, all_words):
-    pass
+    """
+       return bag of words array:
+       1 for each known word that exists in the sentence, 0 otherwise
+       example:
+       sentence = ["hello", "how", "are", "you"]
+       words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+       bag   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
+       """
+
+    # stem each word
+    sentence_words = [stemming(word) for word in token_sentence]
+    # initialize bag with 0 for each word
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx, w in enumerate(all_words):
+        if w in sentence_words:
+            bag[idx] = 1
+    return bag
