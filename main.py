40b2004d8fcd0573ffad59d59ad4b5714dd46a8e
DarkPrinceWarrior <safaev.rus@gmail.com>
2021-12-09 23:58:23 +0300

    optimized model compile parameters

--- main.py
+++ main.py
@@ -1,19 +1,61 @@
 import json
 
-
+import random
 import nltk_utils
 import numpy as np
 import os
 
-from model import model_create, model_compile, model_fit
+from model import model_create, model_compile, model_fit, K_fold_validation
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 from keras.utils.np_utils import to_categorical
+
 
 def jsonOpen():
     with open('intents.json', 'r') as f:
         intents = json.load(f)
     return intents
+
+
+def build_model(tags, all_words, input_size, hidden_size, output_size,
+                loss, metrics, epochs,
+                batch_size):
+
+    # create the model
+    model = model_create(input_size, hidden_size, output_size)
+
+    # compile the model
+    model = model_compile(model, loss, metrics)
+
+    # fit the model and get the results
+
+    x_val = x_train[:3]
+    partial_x_train = x_train[3:]
+
+    y_val = y_train[:3]
+    partial_y_train = y_train[3:]
+
+    model.fit(x_train,
+              y_train,
+              epochs=epochs,
+              batch_size=batch_size, shuffle=True, verbose=1)
+
+
+    # model.fit(partial_x_train,
+    #           partial_y_train,
+    #           epochs=epochs,
+    #           batch_size=batch_size,
+    #           validation_data=(x_val, y_val), shuffle=True, verbose=1)
+
+    sentence = nltk_utils.tokenize("Which items do you have?")
+    sentence = nltk_utils.bag_of_words(sentence, all_words)
+    sentence = np.array(sentence)
+    sentence = sentence.reshape(1, sentence.shape[0])
+
+    prediction = model.predict(sentence)
+    tag_index = np.argmax(prediction)
+    print(tag_index)
+    print(tags[tag_index])
 
 
 if __name__ == "__main__":
@@ -41,47 +83,34 @@ if __name__ == "__main__":
     x_train = []
     y_train = []
 
+    random.shuffle(results)
+
     for (pattern_sentence, tag) in results:
         # X: bag of words for each pattern_sentence
         bag = nltk_utils.bag_of_words(pattern_sentence, all_words)
         x_train.append(bag)
-        # y: PyTorch CrossEntropyLoss needs only class labels, not one-hot
         label = tags.index(tag)
         y_train.append(label)
 
     x_train = np.array(x_train)
-    # y_train = np.array(y_train)
-
+    # print(x_train)
     y_train = to_categorical(y_train, 7)
 
     # create the NN model
     input_size = len(all_words)
     # number of hidden neurons
-    hidden_size = 8
+    hidden_size = 128
     # number of output neurons
     output_size = 7
-    model = model_create(input_size, hidden_size, output_size)
-
-    # compile the model
     loss = "categorical_crossentropy"
     metrics = "accuracy"
-    model = model_compile(model, loss, metrics)
+    epochs = 200
+    batch_size = 5
 
+    # simple build method
+    build_model(tags, all_words, input_size, hidden_size, output_size, loss, metrics, epochs, batch_size)
 
-    # fit the model and get the results
-    x_val = x_train[:5]
-    partial_x_train = x_train[5:]
-
-    y_val = y_train[:5]
-    partial_y_train = y_train[5:]
-    epochs = 2
-    batch_size = 8
-    history = model_fit(model,partial_x_train,partial_y_train,
-                        x_val,y_val,
-                        epochs,batch_size)
-
-
-
-    print(history)
-
-
+    # cross-validation method
+    # K_fold_validation(x_train, y_train,
+    #                   input_size, hidden_size, output_size,
+    #                   loss, metrics, epochs, batch_size)
--- model.py
+++ model.py
@@ -1,36 +1,77 @@
 import os
+
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import numpy as np
 from keras import models
 from keras.applications.densenet import layers
+from keras.layers import Dropout
+from keras.optimizer_v2.gradient_descent import SGD
 
 
-def model_create(input_size,hidden_size,output_size):
+
+def model_create(input_size, hidden_size, output_size):
     model = models.Sequential()
     model.add(layers.Dense(hidden_size, activation="relu", input_shape=(input_size,)))
-    model.add(layers.Dense(hidden_size, activation="relu"))
+    model.add(Dropout(0.5))
+    model.add(layers.Dense(hidden_size-64, activation="relu"))
+    model.add(Dropout(0.5))
     model.add(layers.Dense(output_size, activation="softmax"))
 
     return model
 
+
 def model_compile(model, loss_function, metrics):
-    model.compile(optimizer='rmsprop',
+
+    sgd = SGD(learning_rate=0.01,decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(optimizer=sgd,
                   loss=loss_function,
                   metrics=[metrics])
 
     return model
 
 
-def model_fit(model,partial_x_train,partial_y_train,
-              x_val,y_val,
-              epochs,
-              batch_size):
+def model_fit(model,epochs,batch_size, partial_x_train=None, partial_y_train=None,
+              x_val=None, y_val=None):
+
     history = model.fit(partial_x_train,
                         partial_y_train,
                         epochs=epochs,
                         batch_size=batch_size,
-                        validation_data=(x_val, y_val))
+                        validation_data=(x_val, y_val),shuffle=True, verbose=1)
 
     return history
+
+
+def K_fold_validation(x_train, y_train,
+                      input_size, hidden_size, output_size,
+                      loss_function, metrics, epochs, batch_size):
+    fold_number = 4
+
+    num_samples_fold = len(x_train) // fold_number
+    all_acc = []
+    for i in range(fold_number):
+        val_data = x_train[i * num_samples_fold: (i + 1) * num_samples_fold]
+        val_targets = y_train[i * num_samples_fold: (i + 1) * num_samples_fold]
+
+        partial_train_data = np.concatenate([x_train[:i * num_samples_fold],
+                                             x_train[(i + 1) * num_samples_fold:]],
+                                            axis=0)
+
+        partial_train_targets = np.concatenate([y_train[:i * num_samples_fold],
+                                                y_train[(i + 1) * num_samples_fold:]],
+                                               axis=0)
+
+        model = model_create(input_size, hidden_size, output_size)
+        model = model_compile(model, loss_function, metrics)
+        history = model_fit(model, epochs,batch_size, partial_train_data, partial_train_targets,
+                            val_data, val_targets)
+
+        all_acc.append(history.history["val_accuracy"])
+
+    print(np.mean(all_acc))
+
+
 
 
 def model_evaluation():
